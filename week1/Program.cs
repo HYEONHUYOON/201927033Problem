b429c1c781031e1f2d0345fc3efa368e07024192
BOYDREAMIN <101074004+BOYDREAMIN@users.noreply.github.com>
2023-03-10 12:13:06 +0900

    Add files via upload

---
+++ week1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("숫자를 입력하세요: ");
+        string input = Console.ReadLine();
+        int inputint;
+
+        bool isRight = int.TryParse(input, out inputint);
+        if (isRight) {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            string reversed = new string(chars);
+            Console.WriteLine(reversed);
+        }
+        
+    }
+}
